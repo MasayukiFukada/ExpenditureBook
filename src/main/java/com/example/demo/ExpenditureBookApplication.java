ada6865e52da362c5f2100e1dac8962b8d128141
Masayuki Fukada <m.fukada0813@gmail.com>
2024-05-05 20:25:02 +0900

    :seedling: 初回作成

---
+++ src/main/java/com/example/demo/ExpenditureBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenditureBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenditureBookApplication.class, args);
+	}
+
+}
