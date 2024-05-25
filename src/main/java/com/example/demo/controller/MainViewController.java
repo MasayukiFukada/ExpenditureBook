ab02061eb61deaf98153661b86b95f674c16024a
m.fukada <m.fukada0813@gmail.com>
2024-05-26 07:52:11 +0900

    :wrench: 月の選択を操作しやすいよう逆順に

--- src/main/java/com/example/demo/controller/MainViewController.java
+++ src/main/java/com/example/demo/controller/MainViewController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+import java.time.LocalDate;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,9 +34,14 @@ public class MainViewController {
     public String mainView(Model model) {
         Calendar calendar = Calendar.getInstance();
         int thisYear = calendar.get(Calendar.YEAR);
-        model.addAttribute("year", IntStream.rangeClosed(thisYear - 5, thisYear).toArray());
+        List<Integer> years = IntStream.rangeClosed(thisYear - 5, thisYear).boxed().collect(Collectors.toList());
+        Collections.reverse(years);
+        model.addAttribute("year", years);
 
         aggregateService.Initialize(categoryService);
+
+        LocalDate today = LocalDate.now();
+        model.addAttribute("currentYear", today.getYear());
         return "main.html";
     }
 
